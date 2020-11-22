f9305180615d688d16d5039b05f88423a375a7ca
Xinchuan Du <549174176@qq.com>
2020-11-22 22:51:26 +0800

    Add files via upload
    20201122_version
    .chm file is the "Help" of the CHI660E instrument

---
+++ chi660_macro.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+
+class CHI660e(object):
+	"""docstring for CHI660e"""
+	def __init__(self, arg):
+		super(CHI660e, self).__init__()
+
+		self.arg = arg
+		self.path = os.os.getcwd()    #default file path
+
+	def init_lsv_tech(self, ):
+		pass
+
+	def function():
+		pass
+
+	def function():
+		pass
+		
+
+
+doc=open('output.txt','w')
+
+print("MacroGen")
+CycleNum = 1000
+for loop in range(CycleNum)
+	print("v:"+str(0.5-loop/2000),file=doc)
+	print("run",file=doc)
+	print("save:IT-"+str(loop),file=doc)
+	print("tsave:IT-"+str(loop),file=doc)
+	print(" ",file=doc)
